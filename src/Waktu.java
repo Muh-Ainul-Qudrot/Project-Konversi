0437a20d4bd414817a6269cf25109cd6e0e3b105
ainul <ainul@DESKTOP-PDE9KAJ>
2020-12-16 13:20:29 +0700

    Project Konversi

---
+++ src/Waktu.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ainul
+ */
+import java.util.*;
+public class Waktu {
+   public static void main(String[] args){
+        Scanner Waktu = new Scanner (System.in);
+        int hari, hari1, jam, jam1, menit, menit1, detik, totaldetik;
+        System.out.print("Masukan Total Detik   :");
+        totaldetik = Waktu.nextInt();
+        hari=totaldetik/86400;
+        hari1=totaldetik%86400;
+        jam=hari1/3600;
+        jam1=hari1%3600;
+        menit=jam1/60;
+        menit1=jam1%60;
+        detik=menit1;
+        System.out.println(+hari+" hari : "+jam+" Jam : "+menit+" Menit : "+detik+" Detik");
+    }
+}
+    
+
